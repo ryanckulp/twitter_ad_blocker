12e40a7f2be3734af73d8876f0dbc7951d181e81
Ryan Kulp <ryanckulp@gmail.com>
2021-01-14 10:18:34 +0900

    invert strategy to Promoted > ancestors to fix video, follow btn false positives

--- content.js
+++ content.js
@@ -1,30 +1,18 @@
-let checked_for_ads = false;
 var adsHidden = 0;
 
-setInterval(() => {
-  if (checked_for_ads) {
-    return;
-  }
-
-  const ads = getAds();
-
-  if (ads.length) {
-    ads.forEach(hideAd);
-    checked_for_ads = true;
-  }
-}, 500);
-
 function getAds() {
-  return document.querySelectorAll('div[data-testid=placementTracking]');
+  return Array.from(document.querySelectorAll('span')).filter(el => el.textContent === 'Promoted');
 }
 
 function hideAd(ad) {
-  if (ad.innerText !== 'Follow') {
-    ad.remove();
+  if (ad.closest("div[data-testid=placementTracking]") !== null) { // ignores 'Promoted' follow recos
+    ad.closest("div[data-testid=placementTracking]").remove();
     adsHidden += 1;
     console.log('Twitter ads hidden: ', adsHidden.toString());
+  } else {
+    console.log('Found a promoted something (not tweet), ignored.');
   }
 }
 
-// check for fake news again, if user auto loads more ads in feed
+// re-check as user scrolls
 document.addEventListener('scroll', () => getAds().forEach(hideAd));
