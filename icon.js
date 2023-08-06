6069a9e92990bea203022ab214edcea6ed1881e4
Ryan Kulp <ryanckulp@gmail.com>
2023-08-06 19:03:37 -0400

    bump extension to v3 Manifest style

--- icon.js
+++
@@ -1,3 +0,0 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.create({"url": "https://chrome.google.com/webstore/detail/hide-twitter-ads-block-pr/bapmhjebfdbdpjjfafnkfidijkjlkakf"});
-});
